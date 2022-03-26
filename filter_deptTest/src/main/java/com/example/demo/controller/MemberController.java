af4ad6133daedbf5b8c941c02c2b70d4828516e1
rlawlgusg <iiillliiiili@naver.com>
2022-03-26 23:10:46 +0900

    day0209 Filter의 사용

---
+++ filter_deptTest/src/main/java/com/example/demo/controller/MemberController.java
@@ -0,0 +1,42 @@
+package com.example.demo.controller;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.demo.dao.MemberDao;
+import com.example.demo.vo.MemberVO;
+
+import lombok.Setter;
+
+@Controller
+@Setter
+public class MemberController {
+	@Autowired
+	private MemberDao dao;
+	
+	@RequestMapping(value = "/login",method = RequestMethod.GET)
+	public void login_form() {
+		
+	}
+	
+	@RequestMapping(value = "/login",method = RequestMethod.POST)
+	public ModelAndView login_submit(HttpSession session, String id, String pwd) {
+		ModelAndView mav = new ModelAndView("loginOK");
+		
+		int re = dao.isMember(id, pwd);
+		
+		session.setAttribute("re", re);
+		//re의 값이 1이면 로그인의 상태임
+		
+		if(re != 1) {
+			mav.setViewName("redirect:/login");
+		}
+		return mav;
+	}
+}
